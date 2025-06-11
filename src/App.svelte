314c5614914a565581a2faa8e0fd7aab842f67a2
koohiikouho <115139996+koohiikouho@users.noreply.github.com>
2025-06-12 07:04:42 +0800

    Added Home Page

--- src/App.svelte
+++ src/App.svelte
@@ -3,21 +3,30 @@
   import './app.css';
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, P } from "flowbite-svelte";
   import Home from './routes/Home.svelte';
+  import Lab from './routes/Labs.svelte';
   // import Labs from './routs/Labs.svelte';
   import { slide } from "svelte/transition";
-  const views = [Home];
+  const views = [Home, Lab];
+
+  let navbar = null;
+  let navbarText =null;
 
   let viewportComponent = null;
   let currentView = 0;
 
   function viewHome(){
     currentView = 0;
-    event.preventDefault();
+
+    navbar.add();
     updateViewportComponent();  
   }
 
   function viewLabs(){
     currentView = 1;
+
+
+
+    navbar.add();
     updateViewportComponent();  
   }
 
@@ -30,19 +39,19 @@
 
 </script>
 <header>
-  <div class="text-white w-screen shadow-md hover-transparency">
-    <Navbar class="p-0 fixed start-0 top-0 z-20 bg-primary-400/80">
+  <div id="navbar" class="text-white w-screen shadow-md">
+    <Navbar class="p-0 fixed start-0 top-0 z-20 bg-white/0 hover:bg-surface-50/60 transition">
       <NavBrand href="/">
-        <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Lab Club Logo" />
-        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Lab Club!</span>
+        <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9 text-surface-400" alt="Lab Club Logo" />
+        <span id="navbarText" class="self-center text-xl font-semibold whitespace-nowrap dark:text-white text-surface-400" class:text-white={currentView==1}>Lab Club!</span>
       </NavBrand>
       <NavHamburger />
       <NavUl transition={slide} transitionParams={{ y: -20, duration: 250 }}>
-          <NavLi href="#" class="text-white"> <a on:click={viewHome}> Home </a> </NavLi>
-          <NavLi href="#" class="text-white"> <a on:click={viewLabs}> Labs </a></NavLi>
-          <NavLi href="/docs/components/navbar" class="text-white">About</NavLi>
-          <NavLi href="/pricing" class="text-white">Placeholder</NavLi>
-          <NavLi href="/contact" class="text-white">Placeholder</NavLi>
+          <NavLi href="#" class="text-surface-400"> <a on:click={viewHome} class:text-white={currentView==1}> Home </a> </NavLi>
+          <NavLi href="#" class="text-surface-400"bind:this={navbarText}> <a on:click={viewLabs} class:text-white={currentView==1}> Labs </a></NavLi>
+          <NavLi href="/docs/components/navbar" class="text-surface-400"> <a class:text-white={currentView==1}> About </a></NavLi>
+          <NavLi href="/pricing" class="text-surface-400">Placeholder</NavLi>
+          <NavLi href="/contact" class="text-surface-400">Placeholder</NavLi>
       </NavUl>
     </Navbar>
   </div>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -4,34 +4,66 @@
 </script>
 
 
-<div class="relative flex h-300 w-screen flex-col items-center justify-start overflow-hidden bg-background md:shadow-xl">
+<div class="relative flex h-250 w-screen flex-col items-center justify-start overflow-hidden bg-background md:shadow-xl">
 
-    <img src="../src/assets/logo.png" alt="logo" class="w-auto h-75 mt-50 mb-10">
+    <img src="/src/assets/logo.png" alt="logo" class="w-auto h-75 mt-50 mb-10">
     <span 
-    class="pointer-events-none whitespace-pre-wrap bg-linear-to-tr from-primary-500 to-secondary-100 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10"
+    class="pointer-events-none whitespace-pre-wrap bg-linear-to-br from-primary-200 to-primary-600 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10"
     >
-        Reserve. Click. Done!
+       Click. <span class="bg-linear-to-br from-secondary-200 to-secondary-600 bg-clip-text"> Reserve.</span>   <span class="animated-gradient">Done!</span>
     </span>
     <br>
     <p class="text-tertiary-700 text-3xl">Instant computer lab bookings at your fingertips.</p>
     <div class="-z-10">
         <Particles className="absolute inset-0" refresh={true} />
     </div>
-
-    <div class="flex mt-10 gap-10">
+    
+    <div class="gap-10 mt-10">
         <button type="button" 
         class="text-white bg-gradient-to-r from-primary-200 via-primary-300 to-primary-400 hover:bg-gradient-to-br 
-        focus:ring-4 focus:outline-none focus:ring-primary-300 dark:focus:ring-primary-800 shadow-lg shadow-primary-500/50 dark:shadow-lg 
-        dark:shadow-primary-800/80 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2 pointer-events-auto w-auto h-13">
+        focus:ring-4 focus:outline-none focus:ring-primary-300 dark:focus:ring-primary-800 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2 pointer-events-auto fixcursor">
             Get Started
         </button>
-        <button type="button" 
-        class="text-white bg-gradient-to-r from-primary-200 via-primary-300 to-primary-400 hover:bg-gradient-to-br 
-        focus:ring-4 focus:outline-none focus:ring-primary-300 dark:focus:ring-primary-800 shadow-lg shadow-primary-500/50 dark:shadow-lg 
-        dark:shadow-primary-800/80 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2 pointer-events-auto w-auto h-13">
-            Learn More
-        </button>
 
+
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-lg font-medium text-primary-900 rounded-full group bg-gradient-to-br from-primary-200 to-primary-300
+        group-hover:from-primary-600 group-hover:to-primary-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none 
+        focus:ring-primary-300 dark:focus:ring-primary-800 fixcursor">
+        <span class="relative px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-full  group-hover:bg-transparent group-hover:dark:bg-transparent">
+            Learn More
+        </span>
+        </button>
     </div>
+
+
 </div>
 
+    <div class="bg-primary-200 w-screen h-100">
+
+    </div>
+
+
+<style>
+	.animated-gradient {
+		--bg-size: 400%;
+		--color-one: oklch(86% 0.1 126.06deg);
+		--color-two: oklch(68.53% 0.1 18.56deg);
+		
+
+		background: linear-gradient(
+									90deg,
+									var(--color-one),
+									var(--color-two),
+									var(--color-one)
+								) 0 0 / var(--bg-size) 100%;
+		animation: move-bg 15s infinite linear;
+		color: transparent;
+		-webkit-background-clip: text;
+	}
+
+	@keyframes move-bg {
+		to {
+			background-position: var(--bg-size) 0;
+		}
+	}
+</style>
