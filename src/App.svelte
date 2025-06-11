777cbf7e0395525aa1f64df4469e5e1f4586804d
koohiikouho <115139996+koohiikouho@users.noreply.github.com>
2025-06-12 04:48:03 +0800

    home page almost done

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,55 @@
 <script>
+
   import './app.css';
-  import './lib/components/BackgroundBoxesExample.svelte'
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, P } from "flowbite-svelte";
-  import Lights from './lib/components/Lights.svelte';
+  import Home from './routes/Home.svelte';
+  // import Labs from './routs/Labs.svelte';
+  import { slide } from "svelte/transition";
+  const views = [Home];
+
+  let viewportComponent = null;
+  let currentView = 0;
+
+  function viewHome(){
+    currentView = 0;
+    event.preventDefault();
+    updateViewportComponent();  
+  }
+
+  function viewLabs(){
+    currentView = 1;
+    updateViewportComponent();  
+  }
+
+  function updateViewportComponent() {
+    viewportComponent = views[currentView];
+  }
+  updateViewportComponent();
   
+  // document.getElementById("homebutton").addEventListener("click", viewHome); 
+
 </script>
 <header>
-  
-  <div class="bg-primary-400/80 text-white w-screen shadow-md">
-    <Navbar class="p-0">
+  <div class="text-white w-screen shadow-md hover-transparency">
+    <Navbar class="p-0 fixed start-0 top-0 z-20 bg-primary-400/80">
       <NavBrand href="/">
         <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Lab Club Logo" />
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Lab Club!</span>
       </NavBrand>
       <NavHamburger />
-      <NavUl >
-          <NavLi href="/" class="text-white">Home</NavLi>
-          <NavLi href="/about" class="text-white">Labs</NavLi>
+      <NavUl transition={slide} transitionParams={{ y: -20, duration: 250 }}>
+          <NavLi href="#" class="text-white"> <a on:click={viewHome}> Home </a> </NavLi>
+          <NavLi href="#" class="text-white"> <a on:click={viewLabs}> Labs </a></NavLi>
           <NavLi href="/docs/components/navbar" class="text-white">About</NavLi>
           <NavLi href="/pricing" class="text-white">Placeholder</NavLi>
           <NavLi href="/contact" class="text-white">Placeholder</NavLi>
       </NavUl>
     </Navbar>
   </div>
- 
 </header>
 
-
-
-    <br>
-    <br>
-<div>
- <P>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quos impedit quo, quis quam in distinctio deleniti facere! Ea aliquid maiores iusto obcaecati rerum quisquam repellendus dignissimos rem quo veritatis.</P>
-
-<P>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quos impedit quo, quis quam in distinctio deleniti facere! Ea aliquid maiores iusto obcaecati rerum quisquam repellendus dignissimos rem quo veritatis.</P>
-</div>
-
-<Lights/>
-
+{#if viewportComponent == views[currentView]}
+	<div id="viewport" on:outroend={updateViewportComponent} transition:slide>
+		<svelte:component this={viewportComponent}></svelte:component>
+	</div>
+{/if}
---
+++ src/routes/Home.svelte
@@ -0,0 +1,37 @@
+<script>
+    import Particles from "../lib/components/Particles.svelte";
+    import { Button, GradientButton } from "flowbite-svelte";
+</script>
+
+
+<div class="relative flex h-300 w-screen flex-col items-center justify-start overflow-hidden bg-background md:shadow-xl">
+
+    <img src="../src/assets/logo.png" alt="logo" class="w-auto h-75 mt-50 mb-10">
+    <span 
+    class="pointer-events-none whitespace-pre-wrap bg-linear-to-tr from-primary-500 to-secondary-100 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10"
+    >
+        Reserve. Click. Done!
+    </span>
+    <br>
+    <p class="text-tertiary-700 text-3xl">Instant computer lab bookings at your fingertips.</p>
+    <div class="-z-10">
+        <Particles className="absolute inset-0" refresh={true} />
+    </div>
+
+    <div class="flex mt-10 gap-10">
+        <button type="button" 
+        class="text-white bg-gradient-to-r from-primary-200 via-primary-300 to-primary-400 hover:bg-gradient-to-br 
+        focus:ring-4 focus:outline-none focus:ring-primary-300 dark:focus:ring-primary-800 shadow-lg shadow-primary-500/50 dark:shadow-lg 
+        dark:shadow-primary-800/80 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2 pointer-events-auto w-auto h-13">
+            Get Started
+        </button>
+        <button type="button" 
+        class="text-white bg-gradient-to-r from-primary-200 via-primary-300 to-primary-400 hover:bg-gradient-to-br 
+        focus:ring-4 focus:outline-none focus:ring-primary-300 dark:focus:ring-primary-800 shadow-lg shadow-primary-500/50 dark:shadow-lg 
+        dark:shadow-primary-800/80 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2 pointer-events-auto w-auto h-13">
+            Learn More
+        </button>
+
+    </div>
+</div>
+
