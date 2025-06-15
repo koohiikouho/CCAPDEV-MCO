4b00584878b24ae49c0dd9beb45fe59cdd420286
koohiikouho <115139996+koohiikouho@users.noreply.github.com>
2025-06-15 20:19:31 +0800

    Update LabCard.svelte

--- src/lib/components/LabCard.svelte
+++ src/lib/components/LabCard.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="space-y-4">
-    <Card img={thumbnail} href="src/routes/room/room.html?labCode=1" class="bg-primary-200/50 hover:bg-primary-200 shadow-none transition-shadow duration-100 cursor-pointer hover:shadow-lg hover:shadow-secondary-200/50
+    <Card img={thumbnail} href="src/routes/room/room.html?labCode=1" class="bg-primary-300/50 hover:bg-primary-200 shadow-none transition-shadow duration-100 cursor-pointer hover:shadow-lg hover:shadow-secondary-200/50
     backdrop-blur-md outline-2 outline-dashed outline-secondary-200 " imgClass=" border-b-2 border-dashed border-primary-300/50">
         <div class="m-6">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-offwhite dark:text-white">{labName}</h5>
