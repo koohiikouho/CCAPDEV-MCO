cee26ef16847cbac3153c6d4b2a1c4a46f34c89b
koohiikouho <115139996+koohiikouho@users.noreply.github.com>
2025-06-15 22:11:08 +0800

    Update UserCard.svelte

--- src/lib/components/UserCard.svelte
+++ src/lib/components/UserCard.svelte
@@ -2,13 +2,16 @@
   import { Card, Dropdown, DropdownItem, Avatar, Button } from "flowbite-svelte";
   import { DotsHorizontalOutline } from "flowbite-svelte-icons";
 
-  let { name, email, avatar }: { name: string; email: string; avatar: string } = $props();
+
+  let { name, email, avatar, open = $bindable(false) }: { name: string; email: string; avatar: string, open} = $props();
 
   // Avatar URL fallback if no avatar provided
   let avatarSrc = $derived(avatar || `https://ui-avatars.com/api/?name=${encodeURIComponent(name)}&background=3b82f6&color=fff`);
+
+
 </script>
 
-<Card class="p-4 sm:p-5 md:p-7">
+<Card class="p-4 sm:p-5 md:p-7 min-w-100">
   <div class="flex justify-end">
   </div>
   <div class="flex flex-col items-center pb-4">
@@ -16,7 +19,7 @@
     <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{name}</h5>
     <span class="text-sm text-gray-500 dark:text-gray-400">{email}</span>
     <div class="mt-4 flex space-x-3 lg:mt-6 rtl:space-x-reverse">
-      <Button>View profile</Button>
+      <Button >View profile</Button>
     </div>
   </div>
 </Card>
