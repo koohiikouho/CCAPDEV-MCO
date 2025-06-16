7743e8777697f13042c05aa1e987b10923882c4e
kingbhatss <nk.reyes1@gmail.com>
2025-06-17 00:20:04 +0800

    profile edit done

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,12 @@
   import Home from './routes/Home.svelte';
   import Lab from './routes/Labs.svelte';
   import Users from './routes/Users.svelte';
-  import Reservations from './routes/Reservations.svelte';
   import { fade, fly, slide } from 'svelte/transition';
   import { expoIn } from 'svelte/easing';
   import { Avatar, Dropdown, DropdownHeader, DropdownItem, DropdownGroup } from "flowbite-svelte";
   import { scrollY } from 'svelte/reactivity/window';
 
-  const views = [Home, Lab, Users, Reservations];
+  const views = [Home, Lab, Users];
 
   let params = new URLSearchParams(location.search);
   let viewNumber = Number(params.get("view"));
@@ -89,7 +88,7 @@
               </DropdownHeader>
               <DropdownGroup class="text-white ">
                 <DropdownItem class="hover:text-surface-400" href='/src/routes/login/login.html'>Login</DropdownItem>
-                <DropdownItem class="hover:text-surface-400">Profile</DropdownItem>
+                <DropdownItem class="hover:text-surface-400" href='/src/routes/profiles/viewProfile.html'>Profile</DropdownItem>
                 <DropdownItem class="hover:text-surface-400" onclick={viewReservations}>Reservations</DropdownItem>
               </DropdownGroup>
               <DropdownGroup class="text-white hover:text-surface-400">
