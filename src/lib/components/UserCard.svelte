ca876a24d03face9307ecd2b1fe1a8f6777ebde2
Joshua Gonzales <joshuanicolaigonzales@gmail.com>
2025-06-16 10:38:57 +0800

    Changed User cards to fit with labcards

--- src/lib/components/UserCard.svelte
+++ src/lib/components/UserCard.svelte
@@ -10,14 +10,15 @@
 
 </script>
 
-<Card href="" class="p-4 sm:p-5 md:p-7 min-w-100 bg-primary-300/50 hover:bg-primary-200 shadow-none transition-shadow duration-100 cursor-pointer hover:shadow-lg hover:shadow-secondary-200/50">
+<Card href="" class="p-4 sm:p-5 md:p-7 min-w-100 bg-primary-50/20 rounded-md hover:opacity-80 hover:scale-110 hover:border-[0.5px] hover:border-primary-600 transition-transform shadow-none duration-100 cursor-pointer hover:shadow-lg hover:shadow-secondary-200/50
+    backdrop-blur-md  text-start">
   <div class="flex justify-end">
   </div>
   <div class="flex flex-col items-center pb-4">
     <Avatar size="lg" src={avatarSrc} />
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-offwhite dark:text-white">{name}</h5>
-    <span class="mb-3 leading-tight font-normal text-offwhite/80 dark:text-gray-400">{email}</span>
-    <span class="mb-3 leading-tight font-normal text-offwhite/80 dark:text-gray-400">{role}</span>
+      <h5 class="mb-2 text-2xl tracking-tight text-surface-500 dark:text-white font-medium">{name}</h5>
+      <span class="mb-3 text-xs tracking-tight text-surface-400 dark:text-gray-400 font-normal">{email}</span>
+      <span class="mb-3 text-xs tracking-tight text-surface-400 dark:text-gray-400 font-normal">{role}</span>
     <div class="mt-4 flex space-x-3 lg:mt-6 rtl:space-x-reverse">
     </div>
   </div>
