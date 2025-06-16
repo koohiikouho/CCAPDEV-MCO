80ae632d40185208ceec1b4065e23e7d20c15ea3
Joshua Gonzales <joshuanicolaigonzales@gmail.com>
2025-06-16 08:45:40 +0800

    Finished base of LoginPage

---
+++ src/lib/components/LoginInt.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Section, Register } from 'flowbite-svelte-blocks';
+	import { Button, Checkbox, Label, Input } from 'flowbite-svelte';
+</script>
+
+<Section name="login" class="pt-40">
+	<Register class="max-w-md mx-auto">
+		<div class="space-y-4 p-6 sm:p-8 md:space-y-6">
+			<form class="flex flex-col space-y-6" action="/">
+				<div class="flex items-center justify-center space-x-2">
+					<img class="h-8 w-8" src="/src/assets/logo.png" alt="logo" />
+					<h3 class="p-0 text-xl font-medium text-gray-900 dark:text-white">Login</h3>
+				</div>
+				<Label class="space-y-2">
+					<span>Your email</span>
+					<Input type="email" name="email" placeholder="name@dlsu.edu.ph" required />
+				</Label>
+				<Label class="space-y-2">
+					<span>Your password</span>
+					<Input type="password" name="password" placeholder="•••••" required />
+				</Label>
+				<div class="flex items-start">
+					<Checkbox>Remember me</Checkbox>
+					<a href="/" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Forgot password?</a>
+				</div>
+				<Button type="submit" class="w-full">Sign in</Button>
+				<p class="text-sm font-light text-gray-500 dark:text-gray-400">
+					Don't have an account yet? <a href="/" class="text-primary-600 dark:text-primary-500 font-medium hover:underline">Sign up</a>
+				</p>
+			</form>
+		</div>
+	</Register>
+</Section>
---
+++ src/routes/login/Login.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+    import LoginInt from "../../lib/components/LoginInt.svelte";
+    import {
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+    P,
+    ControlButton,
+    Controls,
+    Hr,
+    Heading,
+    A,
+    Avatar,
+    Dropdown,
+    DropdownHeader,
+    DropdownItem,
+    DropdownGroup,
+    Carousel, 
+    Indicators
+  } from "flowbite-svelte";
+
+  import { AngleRightOutline, ArrowLeftOutline } from "flowbite-svelte-icons";
+
+  let profilePic =
+    "/src/assets/users/alden.jpeg";
+
+  const views = [];
+
+  let navbar = null;
+  let navbarText = null;
+
+  
+
+  let viewportComponent = null;
+  let currentView = 0;
+  let userName = "Kasane Teto";
+  let userEmail = "kasaneteto@utau.com";
+
+  function viewHome() {
+    currentView = 0;
+
+    navbar.add();
+    updateViewportComponent();
+  }
+
+  function viewLabs() {
+    currentView = 1;
+
+    navbar.add();
+    updateViewportComponent();
+  }
+
+  function updateViewportComponent() {
+    viewportComponent = views[currentView];
+  }
+  updateViewportComponent();
+
+</script>
+
+<header>
+  <div id="navbar" class="text-white w-screen shadow-md">
+    <Navbar
+      class="p-0 fixed start-0 top-0 z-20 bg-white/30 hover:bg-surface-50/60 transition backdrop-blur-xl"
+    >
+      <NavBrand href="../../../">
+        <img
+          src="/src/assets/logolite.png"
+          class="me-3 h-6 sm:h-9 text-surface-400 object-fill"
+          alt="Lab Club Logo"
+        />
+        <span
+          id="navbarText"
+          class="self-center text-xl font-semibold whitespace-nowrap dark:text-white text-surface-400"
+          >Lab Club!</span
+        >
+      </NavBrand>
+      <NavHamburger />
+      <NavUl>
+        <NavLi href="../../../index.html?view=1" class="text-surface-400" bind:this={navbarText}>Labs</NavLi>
+        <NavLi href="../../../index.html?view=2" class="text-surface-400">Users</NavLi>
+        <NavLi href="#" class="text-surface-400">About</NavLi>
+        <Avatar id="user-drop" src={profilePic} class="cursor-pointer ml-5" />
+        <Dropdown triggeredBy="#user-drop" class="mt-5 bg-primary-200">
+          <DropdownHeader>
+            <span class="block text-sm text-white">{userName}</span>
+            <span class="block truncate text-sm font-medium text-white"
+              >{userEmail}</span
+            >
+          </DropdownHeader>
+          <DropdownGroup class="text-white">
+            <DropdownItem>Profile</DropdownItem>
+            <DropdownItem>Reservations</DropdownItem>
+          </DropdownGroup>
+          <DropdownGroup class="text-white">
+            <DropdownItem>Sign out</DropdownItem>
+          </DropdownGroup>
+        </Dropdown>
+      </NavUl>
+    </Navbar>
+  </div>
+</header>
+
+<LoginInt />
